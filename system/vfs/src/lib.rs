70a4cfeff934692c179b76c67380560009d9506a
ShivaCoreDev <michael.worob@gmail.com>
2026-09-15 15:34:52 +0200

    style: format VFS mount definitions

--- system/vfs/src/lib.rs
+++ system/vfs/src/lib.rs
@@ -3,15 +3,43 @@
 pub mod persistence;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Mount { pub mountpoint: String, pub filesystem: String, pub readonly: bool }
+pub struct Mount {
+    pub mountpoint: String,
+    pub filesystem: String,
+    pub readonly: bool,
+}
 
 pub fn required_mounts() -> [Mount; 6] {
     [
-        Mount { mountpoint: "/boot".into(), filesystem: "efi".into(), readonly: true },
-        Mount { mountpoint: "/system".into(), filesystem: "system".into(), readonly: true },
-        Mount { mountpoint: "/etc".into(), filesystem: "config".into(), readonly: false },
-        Mount { mountpoint: "/home".into(), filesystem: "data".into(), readonly: false },
-        Mount { mountpoint: "/var".into(), filesystem: "state".into(), readonly: false },
-        Mount { mountpoint: "/run".into(), filesystem: "runtime".into(), readonly: false },
+        Mount {
+            mountpoint: "/boot".into(),
+            filesystem: "efi".into(),
+            readonly: true,
+        },
+        Mount {
+            mountpoint: "/system".into(),
+            filesystem: "system".into(),
+            readonly: true,
+        },
+        Mount {
+            mountpoint: "/etc".into(),
+            filesystem: "config".into(),
+            readonly: false,
+        },
+        Mount {
+            mountpoint: "/home".into(),
+            filesystem: "data".into(),
+            readonly: false,
+        },
+        Mount {
+            mountpoint: "/var".into(),
+            filesystem: "state".into(),
+            readonly: false,
+        },
+        Mount {
+            mountpoint: "/run".into(),
+            filesystem: "runtime".into(),
+            readonly: false,
+        },
     ]
 }
