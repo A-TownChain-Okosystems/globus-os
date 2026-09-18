969e57ac1238fab71dd27c4797289103c4e14be6
ShivaCoreDev <michael.worob@gmail.com>
2026-09-18 10:47:11 +0200

    feat(shivacore): add AMD and Intel CPU HAL

---
+++ modules/atc-shivacore/kernel/src/hal/mod.rs
@@ -0,0 +1,4 @@
+// Copyright (c) 2026 Michael Wroblewski / ShivaCore / A-TownChain-Okosystems. All Rights Reserved.
+//! ShivaCore hardware abstraction layer.
+pub mod cpu;
+pub use cpu::{CpuFeatures, CpuInfo, CpuVendor};
